b1d307435d6efc1da9d5f1835ae1f4f7d07ef333
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2018-01-10 05:27:42 -0800

    add deps to vendor

---
+++ vendor/github.com/sent-hil/go-yamlconfig/yamlconfig.go
@@ -0,0 +1,88 @@
+package yamlconfig
+
+import (
+	"errors"
+
+	yaml "gopkg.in/yaml.v1"
+)
+
+var (
+	// preset environments
+	Development Env = "development"
+	Test        Env = "test"
+	Staging     Env = "staging"
+	Production  Env = "production"
+
+	// name of block that'll be used to look for key, if key is not in env.
+	Default = "defaults"
+
+	ErrKeyNotFound = errors.New("Key not defined for env.")
+)
+
+type Env string
+
+type Client struct {
+	env        Env
+	YamlStruct map[string]interface{}
+}
+
+// New initializes Client after unmarshalling given data arg into yaml.
+func New(data []byte) (*Client, error) {
+	client := &Client{YamlStruct: map[string]interface{}{}}
+
+	if err := yaml.Unmarshal([]byte(data), &client.YamlStruct); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// Get gets given key from set env block. If key is not found in env, it looks
+// in `defaults` blocks. If key doesn't exist there either, it returns
+// ErrKeyNotFound.
+func (r *Client) Get(key string) (interface{}, error) {
+	if v, ok := r.getKeyFromBlock(string(r.GetEnv()), key); ok {
+		return v, nil
+	}
+
+	if v, ok := r.getKeyFromBlock(Default, key); ok {
+		return v, nil
+	}
+
+	return "", ErrKeyNotFound
+}
+
+// GetString gets string value from given key from set env block. Use `Get` for interface return value.
+func (r *Client) GetString(key string) (string, error) {
+	value, err := r.Get(key)
+	if err != nil {
+		return "", err
+	}
+
+	return value.(string), nil
+}
+
+// SetEnv sets given env. This should be used to set env based on env vars or
+// user specified env.
+func (r *Client) SetEnv(env Env) {
+	r.env = env
+}
+
+// GetEnv returns current env. If not set, it returns Development.
+func (r *Client) GetEnv() Env {
+	if r.env == "" {
+		return Development
+	}
+	return r.env
+}
+
+// getKeyFromBlock attempts to get given key from give block. Equivalent to
+// `r.<block>.<key>`.
+func (r *Client) getKeyFromBlock(block, key string) (interface{}, bool) {
+	if v, ok := r.YamlStruct[block]; ok {
+		k, ok := v.(map[interface{}]interface{})[key]
+		return k, ok
+	}
+
+	return nil, false
+}
---
+++ vendor/github.com/sent-hil/go-yamlconfig/yamlconfig_test.go
@@ -0,0 +1,81 @@
+package yamlconfig
+
+import (
+	"log"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var exampleFileStr = `
+defaults: &defaults
+ adapter: postgresql
+ encoding: unicode
+ pool: 5
+ port: 5432
+ host: localhost
+
+development:
+ <<: *defaults
+ database: development
+ username: development
+ password: development
+
+test:
+ <<: *defaults
+ database: test
+ username: test
+ password: test
+`
+
+func TestParsingYamlFile(t *testing.T) {
+	r, err := New([]byte(exampleFileStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Convey("Get", t, func() {
+		Convey("It returns value set for env", func() {
+			val, err := r.Get("database")
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, "development")
+		})
+
+		Convey("It returns error for arg that's not defined", func() {
+			_, err := r.Get("random")
+			So(err, ShouldEqual, ErrKeyNotFound)
+		})
+
+		Convey("It falls back on default if key not defined in env", func() {
+			val, err := r.Get("adapter")
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, "postgresql")
+		})
+	})
+
+	Convey("SetEnv", t, func() {
+		Convey("It returns Development as default if env is not set", func() {
+			So(r.GetEnv(), ShouldEqual, Development)
+		})
+
+		Convey("It returns Test env after Test is set", func() {
+			r.SetEnv(Test)
+			So(r.GetEnv(), ShouldEqual, Test)
+
+			val, err := r.Get("database")
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, "test")
+
+			r.SetEnv(Development) // cleanup
+		})
+	})
+
+	Convey("GetString", t, func() {
+		Convey("It returns string value set for env", func() {
+			val, err := r.GetString("database")
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, "development")
+			So(val, ShouldHaveSameTypeAs, "")
+		})
+	})
+}
